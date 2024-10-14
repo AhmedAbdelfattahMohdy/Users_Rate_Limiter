7db81dfb528d076c0929c35bc0d5b184e5dc64f0
AhmedAbdelfattahMohdy <ahmedmohdy1999@gmail.com>
2024-10-14 03:04:08 +0300

    Refactored folder structure for better organization and project clarity

--- src/main/java/com/mohdy/UsersRateLimiter/Services/RedisService.java
+++ src/main/java/com/mohdy/UsersRateLimiter/Services/RedisService.java
@@ -1,4 +1,4 @@
-package com.mohdy.UsersRateLimiter;
+package com.mohdy.UsersRateLimiter.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
